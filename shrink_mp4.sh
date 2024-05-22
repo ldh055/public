523f0eb913f35e86927922c3828d03c1df157edb
Your Name <you@example.com>
2024-05-22 19:05:28 +0200

    update

--- shrink_mp4.sh
+++ shrink_mp4.sh
@@ -1,3 +1,15 @@
 
-ffmpeg -i piet.mp4 -b 1000000 -crf 28 -s 1080x720 pietg2.mp4
+x=`ls *mp4 | grep -v -e -small` 
+x=${x%.mp4}
+
+echo -e "shrink $x.mp4 ? \c" 
+read ans
+[ "$ans" = "" ] && ans=y
+if [ "$ans" != "y" ] ; then
+ echo " no action " 
+else
+ ffmpeg -i $x.mp4 -b 1000000 -crf 28 -s 1080x720 $x-small.mp4
+fi
+echo HIT ENTER
+read x
 
